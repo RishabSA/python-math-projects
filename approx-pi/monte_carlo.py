e911f9e28e77e2b5c2b3ce34a356b36aca77ae66
Rishab Alagharu <rishabsaia@gmail.com>
2025-04-30 21:43:19 -0400

    Archimedes, Machin, Chudnovsky PI Approx

--- approx-pi/monte_carlo.py
+++ approx-pi/monte_carlo.py
@@ -1,10 +1,10 @@
 import numpy as np
 import math
 
-# The Monte Carlo Approximation of Pi involves random numbers
+# The Monte Carlo Approximation of π involves random numbers
 # We generate random numbers for the x and y falling within [-1, 1] on the x and y axes
 # We then find the number of those points that fall within a circle with a radius of 1 inside of the square
-# We find the ratio of points in the circle to the number of total points and multiply by 4 to get the pi approximation
+# We find the ratio of points in the circle to the number of total points and multiply by 4 to get the π approximation
 
 
 def monte_carlo(n_points: int):
